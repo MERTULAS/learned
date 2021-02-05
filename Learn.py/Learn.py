0a03292da366dc536fdbb3325896b269551e40ab
Mert ULAS <h.mert.ulas@gmail.com>
2021-02-05 22:46:56 +0300

    version v 0.1.1
    learn.js for javascript released and r2score added

--- Learn.py/Learn.py
+++ Learn.py/Learn.py
@@ -1,3 +1,4 @@
+from time import time
 
 
 class GradientDescent:
@@ -14,7 +15,11 @@ class GradientDescent:
     def derivatives_sum_of_squared_residuals(self):
         derivative_intercept = 0
         derivative_slope = 0
-        n = len(self.data)
+        """
+        prediction = self.intercept + self.slope * self.weights
+        derivative_intercept = -2 * np.sum(self.heights - prediction)
+        derivative_slope = -2 * np.sum(self.weights * (self.heights - prediction))
+        """
         for i in range(len(self.data)):
             prediction = self.intercept + self.slope * self.weights[i]
             derivative_intercept += -2 * (self.heights[i] - prediction)
@@ -35,8 +40,12 @@ class GradientDescent:
             sum_of_squared_residuals += (self.heights[i] - prediction) ** 2
         return sum_of_squared_residuals
 
+    def r2_squared(self, first_ssr):
+        return (first_ssr - self.success_rate()) / first_ssr
+
     def optimizer(self, number_of_steps=False):
         first_ssr = self.success_rate()
+        time_start = time()
         if number_of_steps:
             i = 1
             while i <= number_of_steps:
@@ -50,11 +59,15 @@ class GradientDescent:
                 self.minimize_val, slope = self.derivatives_sum_of_squared_residuals()
                 print("Epoch:{}______Derivative_intercept:{}, Derivative_slope:{}".format(i, self.minimize_val, slope))
                 print("___Intercept:{}, Slope:{}".format(self.intercept, self.slope))
-                if 0.1 > self.minimize_val > -0.1:
+                if 0.01 > self.minimize_val > -0.01:
                     break
                 i += 1
+        time_stop = time()
+        benchmark = time_stop - time_start
         print("First SSR:{}".format(first_ssr))
         print("Last SSR:{}".format(self.success_rate()))
+        print("R-Squared:%{}".format(100 * self.r2_squared(first_ssr)))
+        print("Completed in {} seconds".format(benchmark))
 
     def get_parameters(self):
         return self.intercept, self.slope
