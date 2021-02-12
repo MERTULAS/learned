046f5922383748797bc851218ee7105d1239e6c9
Mert ULAS <h.mert.ulas@gmail.com>
2021-02-12 04:32:34 +0300

    version 0.3.1
    made improvements to the code

--- Learn.py/Learn.py
+++ Learn.py/Learn.py
@@ -3,102 +3,94 @@ import numpy as np
 
 
 class GradientDescent:
-    __slots__ = ["data", "slope", "learning_rate", "weights", "heights", "beta", "data_x"]
+    __slots__ = ["learning_rate", "weights", "heights", "beta", "data_x", "cost"]
 
-    def __init__(self, data, slope=1, learning_rate=0.00001):
-        self.data = data
-        self.data_x = np.array(self.data.iloc[:, :-1].values)
+    def __init__(self, data, learning_rate=0.00001):
+        try:
+            self.data_x = np.array(data.iloc[:, :-1].values)
+        except AttributeError:
+            self.data_x = np.array(data[:, :-1])
         self.weights = np.append(np.ones((self.data_x.shape[0], 1)),
                                  self.data_x,
                                  axis=1)
-        self.heights = np.array(self.data.iloc[:, -1:].values)
-        self.slope = slope
+        try:
+            self.heights = np.array(data.iloc[:, -1:].values)
+        except AttributeError:
+            self.heights = np.array(data[:, -1:])
         self.beta = np.append(np.ones((1, 1)) * self.heights.sum() / len(self.heights),
-                              np.ones((self.data_x.shape[1], 1)) * self.slope,
+                              np.zeros((self.data_x.shape[1], 1)),
                               axis=0)
         self.learning_rate = learning_rate
+        self.cost = []
 
-    def derivatives_sum_of_squared_residuals(self):
+    def __derivatives_sum_of_squared_residuals(self):
         prediction = np.dot(self.weights, self.beta)
         pred_gap = prediction - self.heights
-        derivative_intercept = 2 * np.dot(self.weights.T[0],  pred_gap)
-        self.beta[0] = self.beta[0] - derivative_intercept * self.learning_rate
-        self.beta[1:] = self.beta[1:] - (2 * (np.dot(self.weights.T[1:],  pred_gap)) * self.learning_rate)
-        return derivative_intercept
+        self.cost.append((pred_gap ** 2).sum())
+        derivative = 2 * np.dot(self.weights.T,  pred_gap)
+        self.beta = self.beta - (derivative * self.learning_rate)
+        return derivative[0]
 
     @staticmethod
-    def success_rate(weights, heights, beta):
-        prediction = np.dot(weights, beta)
-        pred_gap = heights - prediction
-        sum_of_squared_residuals = (pred_gap ** 2).sum()
-        return sum_of_squared_residuals
-
-    @staticmethod
-    def r2_squared(first_ssr, success_rate):
-        return (first_ssr - success_rate) / first_ssr
-
-    @property
-    def first_ssr(self):
-        intercept_init = self.heights.sum() / len(self.heights)
-        first_beta = np.append(np.ones((1, 1)) * intercept_init,
-                               np.ones((self.data_x.shape[1], 1)) * self.slope,
-                               axis=0)
-        first_ssr = self.success_rate(self.weights, self.heights, first_beta)
-        print(first_ssr)
-        return first_ssr
+    def r2_score(y_true, y_pred):
+        first_ssr = ((y_true - (y_true.sum() / len(y_true))) ** 2).sum()
+        last_ssr = ((y_true - y_pred) ** 2).sum()
+        return (first_ssr - last_ssr) / first_ssr
 
     def optimizer(self, number_of_steps=False):
-        first_ssr = self.first_ssr
         time_start = time()
         if number_of_steps:
             i = 1
             while i <= number_of_steps:
-                self.derivatives_sum_of_squared_residuals()
-                print("Epoch:{}".format(i))
-                i += 1
-        else:
-            i = 1
-            while 1:
-                intercept_derivative = self.derivatives_sum_of_squared_residuals()
+                self.__derivatives_sum_of_squared_residuals()
                 if self.beta[0][0] != self.beta[0][0]:
                     raise Exception("Learning rate should be chosen lower!")
-
-                if 0.000001 > abs(intercept_derivative):
-                    break
+                print("\r", f"Iteration:{i}", end="")
                 i += 1
+        else:
+            i = 0
+            while 1:
+                i += 1
+                intercept_derivative = self.__derivatives_sum_of_squared_residuals()
+                if self.beta[0][0] != self.beta[0][0]:
+                    raise Exception("Learning rate should be chosen lower!")
+                print("\r", f"Intercept Slope:{intercept_derivative}", end="")
+                if i < 2:
+                    continue
+                if 0.0001 > abs(intercept_derivative):
+                    break
         time_stop = time()
         benchmark = round(time_stop - time_start, 2)
-        print("Completed in {} seconds".format(benchmark))
-        success_rate = self.success_rate(self.weights, self.heights, self.beta)
-        print("R-Squared:%{}".format(100 * self.r2_squared(first_ssr, success_rate)))
+        print("\nCompleted in {} seconds".format(benchmark))
+        model_output_predict = np.dot(self.weights, self.beta)
+        r2_score = self.r2_score(self.heights, model_output_predict)
+        print("R-Squared:%{}".format(100 * r2_score))
+
+    def test(self, data):
+        try:
+            weight_x = data.iloc[:, :-1].values
+        except AttributeError:
+            weight_x = data[:, :-1]
+        weight = np.append(np.ones((weight_x.shape[0], 1)),
+                           weight_x,
+                           axis=1)
+        try:
+            height = data.iloc[:, -1:].values
+        except AttributeError:
+            height = data[:, -1:]
+
+        test_data_predict = np.dot(weight, self.beta)
+        r2_score = self.r2_score(height, test_data_predict)
+        print("Test Score: %{}".format(100 * r2_score))
 
     def get_parameters(self):
         return self.beta[0][0], self.beta[1:]
 
-    def test(self, data):
-        """if self.intercept != self.intercept or self.slope != self.slope:
-            raise Exception("This model not trained!")"""
-
-        try:
-            weight_x = data.iloc[:, :-1].values
-            weight = np.append(np.ones((weight_x.shape[0], 1)),
-                               weight_x,
-                               axis=1)
-            height = data.iloc[:, -1:].values
-        except TypeError:
-            weight = data[0]
-            height = data[1]
-        first_ssr = self.first_ssr
-        last_ssr = self.success_rate(weight, height, self.beta)
-        r2_squared = self.r2_squared(first_ssr, last_ssr)
-        print("Test Score: %{}".format(100 * r2_squared))
-
 
 class LinReg:
-    __slots__ = ["data", "x_train", "x", "y", "theta_init", "theta"]
+    __slots__ = ["x_train", "x", "y", "theta_init", "theta"]
 
     def __init__(self, data):
-        self.data = data
         try:
             self.x_train = np.array(data.iloc[:, :-1].values)
         except AttributeError:
@@ -161,11 +153,17 @@ class LinReg:
 
     @property
     def intercept(self):
-        return self.theta[0][0]
+        if self.theta is not None:
+            return self.theta[0][0]
+        else:
+            raise Exception("Model not trained!")
 
     @property
     def coefficients(self):
-        return self.theta[1:].reshape(1, len(self.theta) - 1)
+        if self.theta is not None:
+            return self.theta[1:].reshape(1, len(self.theta) - 1)
+        else:
+            raise Exception("Model not trained!")
 
 
 class Preprocessing:
